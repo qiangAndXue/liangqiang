2288f44b6bbdfa73665424557876755b7d167a6c
maoxian <maoxian@DESKTOP-QU7RAF3>
2018-08-30 15:16:51 +0800

    test

--- store/public/index.php
+++ store/public/index.php
@@ -1,64 +1,42 @@
 <?php
-
-use Phalcon\Loader;
-use Phalcon\Mvc\View;
-use Phalcon\Mvc\Application;
 use Phalcon\Di\FactoryDefault;
-use Phalcon\Mvc\Url as UrlProvider;
-use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
 
+error_reporting(E_ALL);
 
-
-// 注册一个自动加载器
-$loader = new Loader();
-
-$loader->registerDirs(
-    [
-        "../app/controllers/",
-        "../app/models/",
-    ]
-);
-
-$loader->register();
-
-
-
-// 创建一个 DI
-$di = new FactoryDefault();
-
-// 设置视图组件
-$di->set(
-    "view",
-    function () {
-        $view = new View();
-
-        $view->setViewsDir("../app/views/");
-
-        return $view;
-    }
-);
-
-// 设置一个基础URI, 这样所有生成的URI都包含"tutorial"文件夹
-$di->set(
-    "url",
-    function () {
-        $url = new UrlProvider();
-
-        $url->setBaseUri("/financ/");
-
-        return $url;
-    }
-);
-
-
-
-$application = new Application($di);
+define('BASE_PATH', dirname(__DIR__));
+define('APP_PATH', BASE_PATH . '/app');
 
 try {
-    // 处理请求
-    $response = $application->handle();
 
-    $response->send();
+    /**
+     * The FactoryDefault Dependency Injector automatically registers
+     * the services that provide a full stack framework.
+     */
+    $di = new FactoryDefault();
+
+    /**
+     * Read services
+     */
+    include APP_PATH . "/config/services.php";
+
+    /**
+     * Get config service for use in inline setup below
+     */
+    $config = $di->getConfig();
+
+    /**
+     * Include Autoloader
+     */
+    include APP_PATH . '/config/loader.php';
+
+    /**
+     * Handle the request
+     */
+    $application = new \Phalcon\Mvc\Application($di);
+
+    echo $application->handle()->getContent();
+
 } catch (\Exception $e) {
-    echo "Exception: ", $e->getMessage();
+    echo $e->getMessage() . '<br>';
+    echo '<pre>' . $e->getTraceAsString() . '</pre>';
 }
